bb3b9af983b709b4f9463000e1829b14e1472434
saivenkat <saivenkat@DESKTOP-1HVFKNC>
2021-11-29 12:39:23 +0530

    Add project files.

---
+++ EmployeeApp1/Controllers/EmployeeController.cs
@@ -0,0 +1,73 @@
+﻿using EmployeeApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeApp1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+
+        }
+        IEnumerable<EmployeeTB> GetAllEmployee()
+        {
+            using (DBModel db = new DBModel())
+            {
+                return db.EmployeeTBs.ToList<EmployeeTB>();
+            }
+
+        }
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            EmployeeTB emp = new EmployeeTB();
+            if (id != 0)
+            {
+                using (DBModel db = new DBModel())
+                {
+                    emp = db.EmployeeTBs.Where(x => x.EmployeeID == id).FirstOrDefault<EmployeeTB>();
+                }
+            }
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(EmployeeTB emp)
+        {
+            using (DBModel db = new DBModel())
+            {
+                if (emp.EmployeeID == 0)
+                {
+                    db.EmployeeTBs.Add(emp);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (DBModel db = new DBModel())
+            {
+                EmployeeTB emp = db.EmployeeTBs.Where(x => x.EmployeeID == id).FirstOrDefault<EmployeeTB>();
+                db.EmployeeTBs.Remove(emp);
+                db.SaveChanges();
+            }
+            return RedirectToAction("ViewAll");
+        }
+    }
+}
---
+++ EmployeeApp1/Scripts/Script.js
@@ -0,0 +1,40 @@
+﻿function jQueryAjaxPost(form) {
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid()) {
+            $.ajax({
+                type: 'POST',
+                url: form.action,
+                data: new FormData(form),
+                success: function (response) {
+                    $("#firstTab").html(response);
+                    
+                }
+            });
+    }
+    return false;
+}
+
+function Edit(url) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (response) {
+            $("#secondTab").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Edit');
+            $('ul.nav.nav-tabs a:eq(1)').tab('show');
+        }
+    })
+}
+
+function Delete(url) {
+    if (confirm('Are you sure to delete this record ?') == true) {
+        $.ajax({
+            type: 'POST',
+            url: url,
+            success: function (response) {
+                $("#firstTab").html(response);
+               
+            }
+        })
+    }
+}
